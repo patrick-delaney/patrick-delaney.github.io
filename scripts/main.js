4f9e57ff47888cc796cd5a21c0e433b1b312a14b
patrick-delaney <delaneydesigner@gmail.com>
2024-07-21 14:59:22 -0400

    update!

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,84 @@
+
+
+// ----------------------------------------------------------------
 // MENU TOGGLE
 $(document).ready(function() {
+
+  // ----------------------------------------------------------------
+
   // todo - can we just have one toggle action that looks for a toggle class?
   $( '.mmm-burger, .work-toggle' ).on( 'click', function(e) {
     $('html').toggleClass('menu-open'); 
     e.preventDefault();
   } );
 
+  // ----------------------------------------------------------------
 
   // case study open/close toggle
-  $('.case-study').each(function() {
+  $('.cs-wrapper').each(function() {
     $(this).addClass('state-closed');
   });
 
+  // ----------------------------------------------------------------
+
   $( '.cs-header' ).on( 'click', function(e) {
-    $(this).closest('.case-study').toggleClass('state-closed state-open'); 
+    $(this).closest('.cs-wrapper').toggleClass('state-closed state-open'); 
     e.preventDefault();
   } );
 
 
+  // ----------------------------------------------------------------
+  // Throttle function to limit the rate at which sectionScroll is called
+  function throttle(func, delay) {
+      let lastCall = 0;
+      return function() {
+          const now = new Date().getTime();
+          if (now - lastCall >= delay) {
+              lastCall = now;
+              func();
+          }
+      };
+  }
+
+  // ----------------------------------------------------------------
+  // GET HEIGHT OF NAV
+  function headerHeight() {
+    window.headerHeight = $('.section-header').outerHeight();
+    // console.log(headerHeight);
+  }
 
 
+  // ----------------------------------------------------------------
+  // DETECT IF SECTION FULLY IN VIEW
+  $.fn.isInViewport = function() {
+    var header = window.headerHeight;
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop() + 65;
+    // var viewportBottom = viewportTop + $(window).height();
+    return elementTop < viewportTop && elementBottom > viewportTop;
+  };
 
-
-
-  // var scrollTimeout;
-  // var throttle = 250;
-
-  // function isScrolledIntoView(elem) {
-  //   var docViewTop = $(window).scrollTop();
-  //   var docViewBottom = docViewTop + $(window).height();
-
-  //   var elemTop = $(elem).offset().top;
-  //   var elemBottom = elemTop + $(elem).height();
-
-  //   // return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-  //   return ((elemTop <= docViewBottom));
-  // }
-
-  // $(window).scroll(function () {
-  //   if (!scrollTimeout) {
-  //     scrollTimeout = setTimeout(function () {
-  //       $('.cell').each(function () {
-  //         if (isScrolledIntoView(this) === true) {
-  //           $(this).addClass('burger');
-  //           console.log('burger');
-  //         }
-  //       });
+  // ----------------------------------------------------------------
+  // IF SECTION IN VIEWPORT, APPLY THEME DATA ATTR TO NAV STYLE
+  function sectionScroll() {
+    $('section').each(function() {
+      var activeData = $(this).data('theme');
+      if ($(this).isInViewport()) {
+        // $('#fixed-' + activeData).addClass(activeData + '-active');
+        console.log(activeData);
         
-  //       scrollTimeout = null;
-  //     }, throttle);
-  //   }    
-  // });
+        $('section.section-header').removeClass('theme-dark theme-light theme-page').addClass(activeData);
+      }
+    });
+  }
 
+  
 
+  // ----------------------------------------------------------------
+  // THROTTLED ON SCROLL
+  $(window).on('load resize', throttle(headerHeight, 125));
+  $(window).on('scroll resize', throttle(sectionScroll, 125));
 
 
 });
